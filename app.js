49854a7722fea3b114e9e9ab286bd2c0e63587c8
Andrew Birch <ab8218-cp@yahoo.com>
2025-12-25 20:24:48 +0000

    FEAT: added return to top button and scroll functionality

---
+++ app.js
@@ -0,0 +1,9 @@
+function scrollFunction() {// show / hide 'top' button
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+window.onscroll = () => scrollFunction();
